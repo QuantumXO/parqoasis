35b20836347e4a52b0c2c62c90c5c4b05a9a6b73
Sikorskiy Dmitriy <sikorskiy7777@gmail.com>
2023-09-25 15:08:49 +0300

    add SASS, i18n, architecture creation, docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './view/app';
+import i18n from './services/i18n';
+import { I18nextProvider } from 'react-i18next';
+
+import './assets/styles.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App/>
+    </I18nextProvider>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
