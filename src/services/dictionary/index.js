c8322c51106002b0be9f8e8e6c9467bac59a8029
Sikorskiy Dmitriy <sikorskiy7777@gmail.com>
2023-09-28 19:54:48 +0300

    styles, scroll logic

---
+++ src/services/dictionary/index.js
@@ -0,0 +1,11 @@
+import pl from './pl'
+import de from './de'
+import it from './it'
+import en from './en'
+
+export default {
+  en,
+  pl,
+  de,
+  it,
+}
--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,20 @@
-import jquery from 'jquery';
-import dictionary from './dictionary.js';
+import $ from 'jquery';
+import dictionary from './dictionary';
 
-window.$ = window.jQuery = jquery;
+window.$ = window.jQuery = $;
 
-const DEFAULT_LANGUAGE = 'en'
-const SUPPORTED_LANGUAGES = ['en', 'de', 'it', 'pl']
+require('fullpage.js');
+require('fullpage.js/dist/jquery.fullpage.min.css');
+
+const DEFAULT_LANGUAGE = 'en';
+const SUPPORTED_LANGUAGES = ['en', 'de', 'it', 'pl'];
+const TELEPHONES = {
+  en: '+6285654885575',
+  de: '+6285654885575',
+  it: '+6281536085809',
+  pl: '+6285654885575'
+};
+
 
 // data-translate-Key=""
 $.html5Translate = function (lang) {
@@ -15,18 +25,45 @@ $.html5Translate = function (lang) {
 };
 
 function changeLanguage() {
-  const $this = $(this)
-  const lngCode = $this.attr('data-lng')
+  const $this = $(this);
+  const lngCode = $this.attr('data-lng');
   $.html5Translate(lngCode);
+  
+  handleTelephone(lngCode);
+  
+  localStorage.setItem('language', lngCode);
+}
+
+function handleTelephone(lngCode = 'en') {
+  const phone = TELEPHONES[lngCode];
+  $('#telephone').html(`<a href="tel:${phone}" class="phone">${phone}</a>`);
 }
 
 $(function () {
   const userLanguage = navigator?.language || navigator?.userLanguage;
   const userLanguageIsoCode = userLanguage?.split('-')[0];
-  const isSupportedUserLanguage = SUPPORTED_LANGUAGES.includes(userLanguageIsoCode)
-  const initialLanguage = isSupportedUserLanguage ? userLanguageIsoCode : DEFAULT_LANGUAGE
+  const isSupportedUserLanguage = SUPPORTED_LANGUAGES.includes(userLanguageIsoCode);
+  const selectedUserLanguage = localStorage.getItem('language');
+  let initLanguage = selectedUserLanguage;
   
-  $.html5Translate(initialLanguage);
+  if (!selectedUserLanguage) {
+    initLanguage = isSupportedUserLanguage ? userLanguageIsoCode : DEFAULT_LANGUAGE;
+  }
   
-  $('.language').on('click', changeLanguage)
+  $.html5Translate(initLanguage);
+  handleTelephone(initLanguage);
+  
+  if (window.innerWidth >= 768) {
+    $('#fullpage').fullpage({
+      paddingTop: '48px',
+      autoScrolling: true,
+      scrollHorizontally: false,
+      navigation: true,
+      scrollBar: true,
+      sectionSelector: '.scroll',
+      lazyLoading: true,
+    });
+  }
+  
+  $('.language').on('click', changeLanguage);
 });
